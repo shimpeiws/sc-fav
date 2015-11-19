3a6af4db83cae2f6ef36a0cff153f9723a5a763a
shimpeiws <shimpeiws@gmail.com>
2015-11-19 19:37:12 +0900

    Add Authentication

--- src/renderer/components/main.jsx
+++ src/renderer/components/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import SC from 'soundcloud';
 
 export class Main extends React.Component {
     constructor() {
@@ -8,16 +7,6 @@ export class Main extends React.Component {
             message: 'This is main.jsx',
             trackName: 'default'
         };
-        SC.initialize({
-            client_id: '964d428beee7b8d4350fc942c594fd7d',
-            redirect_uri: 'http://shimpei.ws/sc-fav/callback'
-        });
-        // TODO: Use API Directory(refs: https://github.com/gillesdemey/Cumulus/blob/master/index.js#L50)
-        // SC.connect().then(() => {
-        //     return SC.get('/me');
-        // }).then(function(me) {
-        //     alert('Hello, ' + me.username);
-        // });
     }
     
     render() {
